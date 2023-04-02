34309e57975bab054d80832ad4b60c59989be407
Vivek Kumar <kumarvivek1752@gmail.com>
2023-04-02 08:58:07 +0530

    Q1 with triplets

--- arrays/target_sum.cpp
+++ arrays/target_sum.cpp
@@ -5,10 +5,10 @@
 
  int main()
  {
-    int array[]= {3,4,6,7,1};
+    int array[]= {3,4,6,7,1,2,2};
     int targetsum = 7;
 
-    int size = 5;
+    int size = 7;
     int pairs = 0;
 
     for (int i=0; i<size; i++){
@@ -19,6 +19,26 @@
         }
     }
     cout<<"No of pairs: "<<pairs<< endl;
+
+
+
+    // count the number of triplets whose sum is equal to the given value of X.
+    int targetsumTriplets= 10;
+    int triplets = 0;
+    
+
+    for(int i=0; i<size; i++){
+        for (int j=i+1; j<size; j++){
+            for (int k=j+1; k<size; k++){
+                if (array[i]+ array[j] +array[k] == targetsumTriplets){
+                    triplets++;
+                }
+            }
+        }
+    }
+
+    cout << "No of Triplets : "<< triplets<< endl;
+
 
 
 
