43ceb363171d80b0e21bc2f34ef31c26da4e663b
Vivek Kumar <Kumarvivek1752@gmail.com>
2023-05-19 11:28:30 +0530

    problems in arrays

--- arrays/Two Pointers Problems/short_even_and_odd_integers.cpp
+++ arrays/Two Pointers Problems/short_even_and_odd_integers.cpp
@@ -3,3 +3,54 @@
 // relative order of odd or even integers does not matter. Return
 // any array that satisfies the condition.
 // 
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void shortByParity(vector<int> &v){
+    int left_ptr=0;
+    int right_ptr=v.size()-1;
+
+    while(left_ptr<right_ptr){
+        
+        if(v[left_ptr]%2==1 && v[right_ptr]%2==0){
+            swap(v[left_ptr],v[right_ptr]);
+            left_ptr++; right_ptr--;
+        
+        }
+
+        if(v[left_ptr]%2==0){
+            left_ptr++;
+        }
+        if(v[right_ptr]%2==1){
+            right_ptr--;
+        }
+    }
+    return;
+}
+
+int main (){
+
+    int n;
+    cin>>n;
+    vector<int> v;
+    
+    for(int i=0; i<n; i++){
+        int ele;
+        cin>>ele;
+        v.push_back(ele);
+    }
+
+    shortByParity(v);
+
+    for(int i=0; i<n; i++){
+        cout<<v[i]<<" ";
+
+    }
+    cout<<endl;
+
+    
+    return 0;
+
+}
